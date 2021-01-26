9a21e8bd05dff40f53c897b1589961549cd9934f
Noel Rostohar <noel.rostohar@gmail.com>
2021-01-26 14:25:44 +0100

    Refactored To Sequelize

---
+++ config/database.js
@@ -0,0 +1,11 @@
+const Sequelize = require('sequelize');
+
+module.exports = new Sequelize(
+  process.env.DATABASE,
+  process.env.USERNAME,
+  process.env.PASSWORD,
+  {
+    host: process.env.HOST,
+    dialect: 'mysql',
+  }
+);
